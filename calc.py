4f1aff7bd9e07ab79e7e107e7ffd443e8508e6af
slbsh <crafter.antek@gmail.com>
2026-03-10 21:56:06 +0100

    python

---
+++ calc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+while True:
+	stack = []
+	for e in input().split(" "):
+		if e.isdigit() or e.startswith("-") and e[1:].isdigit():
+			stack.append(int(e))
+			continue
+
+		if len(stack) < 2:
+			print("Too Few Arguments")
+			break
+
+		b, a = stack.pop(), stack.pop()
+
+		match e:
+			case "+": stack.append(a + b)
+			case "-": stack.append(a - b)
+			case "*": stack.append(a * b)
+			case "/": stack.append(a // b)
+			case _:
+				print("Invalid Token")
+				break
+
+	if len(stack) == 1:
+		print(stack[0])
+	else:
+		print("Invalid Input")
+	
